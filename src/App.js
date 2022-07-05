cf9311e758b85dacd0b1dc034ce9397a9b31746a
Robin Punnoose <robin.punnoose@protonmail.com>
2022-07-04 22:36:27 -0500

    created initialstate in App.js

--- src/App.js
+++ src/App.js
@@ -93,23 +93,25 @@ const particleOptions = {
   detectRetina: true,
 }
 
+const initialState = {
+    input: '',
+    imageURL: '',
+    box: {},
+    route: 'signin',
+    isSignedIn: false,
+    user: {
+      id: '',
+      name: '',
+      email: '',
+      entries: 0,
+      joined: ''         
+  }
+}
+
 class App extends Component {
   constructor() {
     super()
-    this.state = {
-      input: '',
-      imageURL: '',
-      box: {},
-      route: 'signin',
-      isSignedIn: false,
-      user: {
-        id: '',
-        name: '',
-        email: '',
-        entries: 0,
-        joined: ''       
-      }
-    }
+    this.state = initialState
   }
 
   loadUser = data => {
@@ -164,6 +166,7 @@ class App extends Component {
           .then(count => {
             this.setState(Object.assign(this.state.user, {entries: count}))
           })
+          .catch(console.log)
       }
       this.displayFaceBox(this.calculateFaceLocation(response))
     })
@@ -172,7 +175,7 @@ class App extends Component {
 
   onRouteChange = (route) => {
     if (route === 'signout') {
-      this.setState({isSignedIn: false})
+      this.setState(initialState)
     } else if(route === 'home') {
       this.setState({isSignedIn: true})
     }

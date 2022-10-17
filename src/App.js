38db0859f5905b5b61d31660d646578fcf68d192
Robin Punnoose <robin.punnoose@protonmail.com>
2022-10-16 19:48:04 -0500

    updated background

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, {Component} from 'react';
-import Particles from "react-tsparticles";
-import { loadFull } from "tsparticles";
+import ParticlesBg from "particles-bg";
 import SignIn from './components/SignIn/SignIn'
 import Register from './components/Register/Register'
 import Navigation from './components/Navigation/Navigation.js'
@@ -10,83 +9,6 @@ import Rank from './components/Rank/Rank.js'
 import FaceRecognition from './components/FaceRecognition/FaceRecognition.js'
 import './App.css';
 
-
-const particlesInit = async (main) => {
-  console.log(main);
-  await loadFull(main);
-};
-
-const particlesLoaded = (container) => {
-  console.log(container);
-};
-
-const particleOptions = {
-  fpsLimit: 30,
-  interactivity: {
-    events: {
-      onClick: {
-        enable: true,
-        mode: "push",
-      },
-      onHover: {
-        enable: true,
-        mode: "repulse",
-      },
-      resize: true,
-    },
-    modes: {
-      push: {
-        quantity: 4,
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4,
-      },
-    },
-  },
-  particles: {
-    color: {
-      value: "#ffffff",
-    },
-    links: {
-      color: "#ffffff",
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1,
-    },
-    collisions: {
-      enable: true,
-    },
-    move: {
-      direction: "none",
-      enable: true,
-      outModes: {
-        default: "bounce",
-      },
-      random: false,
-      speed: 1,
-      straight: false,
-    },
-    number: {
-      density: {
-        enable: true,
-        area: 800,
-      },
-      value: 40,
-    },
-    opacity: {
-      value: 0.5,
-    },
-    shape: {
-      type: "circle",
-    },
-    size: {
-      value: { min: 1, max: 5 },
-    },
-  },
-  detectRetina: true,
-}
 
 const initialState = {
     input: '',
@@ -183,11 +105,7 @@ class App extends Component {
   render () {
     return (
       <div className="App">
-        <Particles className="particles"
-          init={particlesInit}
-          loaded={particlesLoaded}
-          options={particleOptions}
-        />
+       <ParticlesBg type="circle" bg={true} />
         <Navigation isSignedIn={this.state.isSignedIn} onRouteChange={this.onRouteChange}/>
         { this.state.route === 'home' 
           ? <>
